24ead99d9634c7c5af6e85cbde34075d3bdea9a7
Jon <jonegeland@gmail.com>
2018-10-05 14:28:49 -0400

    Order submissions by created_at on the organizers view.

--- src/events_web/controllers/organizer/submissions_controller.cr
+++ src/events_web/controllers/organizer/submissions_controller.cr
@@ -1,9 +1,7 @@
 class Organizer::SubmissionsController < EventsWebController
   def index
     submissions = Events.list_submissions(Query.
-      order_by("game_id ASC").
-      order_by("account_id ASC").
-      order_by("category_name ASC").
+      order_by("created_at ASC").
       preload([:account, :event, :game])
     )
 
